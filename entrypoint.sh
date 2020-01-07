02936b967e08d20b01815e93ab9b77ce6be1a8cf
Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>
2020-01-07 10:24:03 +0100

    Use local dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 
-echo "Pulling docker image"
-docker pull bermeitingerb/texlive:$TEXLIVE
-
-echo "/github/workspace"
-ls /github/workspace
-
-echo "pwd"
-ls $(pwd)
-
-cd /github/workspace
-
 docker run \
 	--rm \
-	--volumes-from / \
     -v "/github/workspace:/data" \
     bermeitingerb/texlive:$TEXLIVE \
     latexmk -r latexmkrc
