6dfeeec095cc4db6ab155152bea0b036766a4a04
Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>
2020-01-07 11:01:38 +0100

    entrypoint edit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ docker run \
 	-w "/data" \
     -v "/github/workspace:/data" \
     bermeitingerb/texlive:$TEXLIVE \
-    bash -c "cd /data && latexmk"
+    bash -c "cd /data && latexmk -r latexmkrc"
