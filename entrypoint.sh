5fe3af23d4539110f4c11d023977f2cc1f065d6b
Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>
2020-01-07 11:46:37 +0100

    also zip files

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,10 @@ docker run \
 	--rm \
 	--volumes-from $(basename $(cat /proc/1/cpuset)) \
 	bermeitingerb/texlive:$TEXLIVE \
-    bash -c "cd /github/workspace && latexmk"
+    bash -c \
+    	"cd /github/workspace && \\
+    	 latexmk && \\
+    	 cp main.pdf /main-$TEXLIVE.pdf && \\
+    	 latexmk -C && \\
+    	 zip -r --exclude='*.git*' --exclude='*.circleci*' --exclude='latexmkrc' --exclude='README.md' /arxiv-$TEXLIVE.zip .
+    "
