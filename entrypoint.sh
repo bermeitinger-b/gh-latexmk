fd34070fbd52969f52e50fd3ac951f00f82da0a3
Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>
2020-01-08 11:10:55 +0100

    Do not add bbl into workspace but add later into zip
    Signed-off-by: Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,11 @@ docker run \
 	--volumes-from $(basename $(cat /proc/1/cpuset)) \
 	-v "/out:/out" \
 	bermeitingerb/texlive:$TEXLIVE \
-    bash -c \
-    	"cd /github/workspace && \\
-    	 rm -rf /build && \\
-         latexmk -outdir=/build -C && \\
-    	 latexmk -outdir=/build && \\
-    	 cp /build/*.bbl . && \\
-	 cp /build/*.pdf /out/ && \\
-    	 zip -r --exclude='*.git*' --exclude='latexmkrc' --exclude='README.md' /out/main.zip ."
+    bash -c "\
+        cd /github/workspace && \\
+        latexmk -outdir=/build && \\
+        cp /build/*.pdf /out/ && \\
+        zip -r --exclude='*.git*' --exclude='latexmkrc' --exclude='README.md' /out/main.zip . && \\
+        zip /out/main.zip -j /build/*.bbl
+    "
 
